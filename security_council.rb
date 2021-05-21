8b4d12354b6b79a8014879f69a04d875e97aa202
akshay-17 <43988784+akshay-17@users.noreply.github.com>
2021-05-21 08:05:08 +0530

    Revert "Module assignment code added"

--- security_council.rb
+++
@@ -1,9 +0,0 @@
-module Security_council_status
-    def seat_in_un(country)
-        if country.population>36000000  && country.state_of_country=="Developed"
-            puts "seat in un"
-        else country.gdp<36000000 &&  country.state_of_country=="Developing"
-            puts "no seat in un"
-        end
-    end
- end
